29859850f3f37fb533f7eb12b697b7647c660642
perdenis <perdomodenis@outlook.com>
2026-05-30 15:56:06 +0200

    quick fix

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-      <NotificationProvider>
+      <EventsProvider>
         <App />
-      </NotificationProvider>
+      </EventsProvider>
     </AuthProvider>
   </React.StrictMode>
 );
